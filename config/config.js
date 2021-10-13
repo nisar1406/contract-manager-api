3114bd8741cf1e846de3168658a1e2e0854bdbe0
NisarShaikh <43024439+nisar1406@users.noreply.github.com>
2021-10-13 18:04:34 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+require( "dotenv" ).config();
 module.exports = {
   "development": {
     "username": "pamptgfk",
@@ -5,19 +6,5 @@ module.exports = {
     "database": "pamptgfk",
     "host": "fanny.db.elephantsql.com",
     "dialect": "postgres"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
   }
 }
