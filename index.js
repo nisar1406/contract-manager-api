2bbfe41b9ef930933662d196c822ca7b98aec126
Nisar Shaikh <>
2021-12-17 15:23:58 +0530

    adds cors

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
-import userRoutes from'./routes/user';
+import userRoutes from './routes/user';
 import contractRoutes from './routes/contract';
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
